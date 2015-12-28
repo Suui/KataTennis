f241bc785561da5703f8e16185ecf53be60cf8d4
Suui <suui.gd@gmail.com>
2015-12-28 20:29:03 +0100

    Green: set_advantage_true_when_adding_a_fourth_point

--- Test/ScoreShould.cs
+++ Test/ScoreShould.cs
@@ -40,27 +40,55 @@ namespace Test
 
 			score.Points.Should().Be(40);
 		}
+
+		[Test]
+		public void set_advantage_true_when_adding_a_fourth_point()
+		{
+			var score = new Score();
+
+			score.AddPoint();
+			score.AddPoint();
+			score.AddPoint();
+			score.AddPoint();
+
+			score.Points.Should().Be(40);
+			score.Advantage.Should().BeTrue();
+		}
 	}
 
 	public class Score
 	{
 		public int Points { get; private set; }
+		public bool Advantage { get; private set; }
+
 		private readonly Dictionary<int, int> _pointAdditions;
 
 		public Score()
 		{
+			Points = 0;
+			Advantage = false;
+
 			_pointAdditions = new Dictionary<int, int>
 			{
 				{ 0, 15 },
 				{ 15, 30 },
 				{ 30, 40 }
 			};
-			Points = 0;
 		}
 
 		public void AddPoint()
 		{
+			if (Points == 40)
+			{
+				SetAdvantage(true);
+				return;
+			}
 			Points = _pointAdditions[Points];
+		}
+
+		private void SetAdvantage(bool value)
+		{
+			Advantage = value;
 		}
 	}
 }
